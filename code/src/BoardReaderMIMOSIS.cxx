f723f9fb4fcf047d81b02b9b2fa66eeaabd938a4
zelbitar <ziad.elbitar@iphc.cnrs.fr>
2021-05-26 08:09:25 +0200

    move data directory to taf directory in BoardReaderMimosis

--- code/src/BoardReaderMIMOSIS.cxx
+++ code/src/BoardReaderMIMOSIS.cxx
@@ -98,7 +98,7 @@ int BoardReaderMIMOSIS::test() {
     
     printf ( "\n" );
     printf ( "**********************************************************\n" );
-    printf ( "Beginning of Mimossi 1 data run files access toto library demo \n" );
+    printf ( "Beginning of Mimosis 1 data run files access toto library demo \n" );
     printf ( "**********************************************************\n" );
     printf ( "\n" );
   
